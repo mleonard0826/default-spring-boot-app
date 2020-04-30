0fa9ae0d838980afe6cdea22b8dca40cb4e10b6a
Michael Leonard <MichaelA.Leonard@LibertyMutual.com>
2020-04-30 08:17:55 -0400

    Logging for tests.

--- src/test/java/mal/projects/defaultspringbootapp/DefaultSpringBootAppApplicationTests.java
+++ src/test/java/mal/projects/defaultspringbootapp/DefaultSpringBootAppApplicationTests.java
@@ -3,23 +3,31 @@ package mal.projects.defaultspringbootapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static org.junit.Assert.*;
 
 @SpringBootTest
 class DefaultSpringBootAppApplicationTests {
 
+	private static final Logger LOGGER = LogManager.getLogger(DefaultSpringBootAppApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOGGER.info("This is the default test...");
 	}
 
 	@Test
 	void addition() {
+		LOGGER.info("This is the addition test...");
 		assertEquals(2, MathUtil.add(1, 1));
 	}
 
-	@Test
-	void badAddition() {
-		assertEquals(8, MathUtil.add(4, 7));
-	}
+//	@Test
+//	void badAddition() {
+//		LOGGER.info("This is the bad addition test...");
+//		assertEquals(8, MathUtil.add(4, 7));
+//	}
 
 }
