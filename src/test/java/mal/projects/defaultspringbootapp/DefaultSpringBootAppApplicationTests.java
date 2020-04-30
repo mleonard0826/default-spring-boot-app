e25441f8b79086482f022fedd6209695ecaec6dd
Michael Leonard <MichaelA.Leonard@LibertyMutual.com>
2020-04-30 07:55:09 -0400

    Added math tests.

--- src/test/java/mal/projects/defaultspringbootapp/DefaultSpringBootAppApplicationTests.java
+++ src/test/java/mal/projects/defaultspringbootapp/DefaultSpringBootAppApplicationTests.java
@@ -3,11 +3,23 @@ package mal.projects.defaultspringbootapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.*;
+
 @SpringBootTest
 class DefaultSpringBootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addition() {
+		assertEquals(2, MathUtil.add(1, 1));
+	}
+
+	@Test
+	void badAddition() {
+		assertEquals(8, MathUtil.add(4, 7));
 	}
 
 }
