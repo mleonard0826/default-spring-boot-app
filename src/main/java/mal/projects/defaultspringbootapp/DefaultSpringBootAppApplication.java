f82e083bf30d372593ada40cbeb75535d1161d83
Michael Leonard <MichaelA.Leonard@LibertyMutual.com>
2020-04-30 06:51:39 -0400

    Initial commit

---
+++ src/main/java/mal/projects/defaultspringbootapp/DefaultSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package mal.projects.defaultspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultSpringBootAppApplication.class, args);
+	}
+
+}
